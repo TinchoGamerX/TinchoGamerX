a2f2b3f6a28fcedb4fa8859bf418a44026f1381a
TinchoGamerX <martinboomperu@gmail.com>
2025-02-25 10:51:51 -0500

    Corrección en configuración de Vite

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173, // Asegura que el puerto sea el correcto
-  },
-  publicDir: 'public', // Asegura que la carpeta public se incluya en la compilación
+  build: {
+    outDir: 'dist', // Asegura que Vite genere los archivos en 'dist'
+  }
 });
